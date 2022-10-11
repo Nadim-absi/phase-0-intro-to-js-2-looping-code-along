267386ec2419eab338c7b5bcaf4989beb1f7a55d
Nadim Absi <absinad1@gmail.com>
2022-10-11 14:03:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function writeCards(arr,eventName){
+    const cardArray = []
+    for (let i = 0; i < arr.length; i++) {
+        cardArray.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return cardArray
+}
+function countDown(number){
+    let i =number;
+    while (i>-1) {
+        console.log(i)
+        i--;
+    }
+}
+countDown(10)
+
 // Code your solutions in this file
